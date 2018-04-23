f2f48b4a7825802f1277dd2bd6211bcaa4348f91
Belinda <bywong@csu.fullerton.edu>
2018-04-22 21:35:16 -0700

    completed EmberQuickstart tutorial

---
+++ .eslintrc0.js
@@ -0,0 +1,31 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "no-console": "off",
+    "no-undef": "off",
+    "no-unused-vars": "off",
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  },
+  "parserOptions": {
+    "sourceType": "module"
+  }
+};
